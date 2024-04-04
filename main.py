5abe0b60683a05da161b30dd38930f381403c6bc
jess <46928740+jessfin@users.noreply.github.com>
2024-04-04 20:17:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,34 +4,26 @@ import json
 import time
 
 with sync_playwright() as p:
-    # 创建一个新的浏览器实例
+
     browser = p.firefox.launch()
 
-    # 打开新的页面
     page = browser.new_page()
 
-    # 打开登录页面
     page.goto('https://www.nodeseek.com/signIn.html')
 
-    # 找到邮箱输入框并输入数据
-    page.fill('#stacked-email', 'jess', timeout=60000)
+    page.fill('#stacked-email', 'username')
 
-    # 找到密码输入框并输入数据
-    page.fill('#stacked-password', 'xjlxjl123', timeout=60000)
+    page.fill('#stacked-password', 'password')
 
-    # 找到登录按钮并点击
     page.click('//button[@type="submit"]')
 
-    # 等待10秒，让JavaScript有足够的时间设置cookie
     time.sleep(60)
 
-    # 获取cookies
     cookies = page.context.cookies()
 
-    # 转换cookies为headers中的"Cookie"字段可以使用的格式
     cookie_str = "; ".join([f"{cookie['name']}={cookie['value']}" for cookie in cookies])
 
-    # 现在你可以使用requests库发送POST请求
+  
     url = "https://www.nodeseek.com/api/attendance?random=true"
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0",
@@ -43,5 +35,4 @@ with sync_playwright() as p:
 
     print(data["message"])
 
-    # 关闭浏览器实例
     browser.close()
