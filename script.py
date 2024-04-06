44cc1dc7f576bd4f94de91f13ceccf5515d4b794
jess <46928740+jessfin@users.noreply.github.com>
2024-04-06 14:10:26 +0800

    Update script.py

--- script.py
+++ script.py
@@ -1,21 +1,23 @@
-import os
 import requests
 import json
 
-# Get the session from environment variables
-session = os.getenv('SESSION')
-
 # Your cookies
-cookie_str = f"session={session}"
+cookies = [
+    "session=a78d8cdbb42df588",
+ #  "session=84a78d8",
+ #   "session=2684a78d8",  // 修改session后面的值，如果需要多用户请删除前面#
+]
 
 # Now you can use requests library to send POST request
 url = "https://www.nodeseek.com/api/attendance?random=true"
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36 Edg/123.0.0.0",
-    "Cookie": cookie_str
 }
-response = requests.post(url, headers=headers)
 
-data = json.loads(response.text)
+for cookie in cookies:
+    headers["Cookie"] = cookie
+    response = requests.post(url, headers=headers)
 
-print(data["message"])
+    data = json.loads(response.text)
+
+    print(data["message"])
